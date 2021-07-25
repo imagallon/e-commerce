702c8640b7ae04b2e6e1872c938b8e45bec99001
Israel Magallon <browncans@gmail.com>
2021-07-24 21:06:24 -0700

    almost done with all the routes. Still need to work on post and put for some though

--- models/index.js
+++ models/index.js
@@ -19,8 +19,7 @@ Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
     unique: false
-  },
-  as: "product_id"
+  }
 })
 
 // Tags belongToMany Products (through ProductTag)
@@ -28,8 +27,7 @@ Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
     unique: false
-  },
-  as: 'tag_id'
+  }  
 })
 
 module.exports = {
