ba837ff7c0b792efc2795f99c0f68ed5420d3d68
Israel Magallon <browncans@gmail.com>
2021-07-24 17:50:14 -0700

    added models product, productTag, and associations in index.js

--- models/Product.js
+++ models/Product.js
@@ -9,65 +9,36 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
     product_name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     price: {
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
       defaultValue: 1,
-      isDecimal: true
+      isDecimal: true,
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
-      isNumeric: true
+      isNumeric: true,
     },
     category_id: {
       type: DataTypes.INTEGER,
       references: {
         model: "category",
         key: "id",
-        unique: false
-      }
+        unique: false,
+      },
     },
-    location_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "location",
-        key: "id",
-        unique: false
-      }
-    }
-    //     id
-    // Integer.
-    // Doesn't allow null values.
-    // Set as primary key.
-    // Uses auto increment.
-    // product_name
-    // String.
-    // Doesn't allow null values.
-    // price
-    // Decimal.
-    // Doesn't allow null values.
-    // Validates that the value is a decimal.
-    // stock
-    // Integer.
-    // Doesn't allow null values.
-    // Set a default value of 10.
-    // Validates that the value is numeric.
-    // category_id
-    // Integer.
-    // References the Category model's id.
   },
   {
     sequelize,
